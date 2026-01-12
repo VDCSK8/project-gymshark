8ba937873e19538fee20245dfa475c9f356102d5
VIEN DINH CUONG <cuongvien13@gmail.com>
2026-01-12 23:00:22 +0700

    9. Làm phần contact

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,4 +1,71 @@
+import GoToTop from "../../components/GoToTop";
+import styles from "./Contact.module.scss";
+
 function Contact() {
-  return <h1>Contact</h1>;
+  return (
+    <main className={styles.contact}>
+      <div className={styles.container}>
+        {/* Header */}
+        <div className={styles.contact__header}>
+          <h2 className={styles.contact__title}>CONTACT & SUPPORT</h2>
+          <h3 className={styles.contact__subtitle}>
+            How can we help? <br />
+            We’re here to support you with orders, products and training gear.
+          </h3>
+        </div>
+
+        {/* Support section */}
+        <div className={styles.support}>
+          <div className={styles.support__list}>
+            {/* Item 1 */}
+            <div className={styles.support__item}>
+              <h3 className={styles.support__itemTitle}>EMAIL</h3>
+              <a
+                href="https://support.gymshark.com/en-US"
+                className={styles.support__link}
+              >
+                support@gymshark.com
+              </a>
+            </div>
+
+            {/* Item 2 */}
+            <div className={styles.support__item}>
+              <h3 className={styles.support__itemTitle}>DELIVER</h3>
+              <a
+                href="https://support.gymshark.com/en-US/article/problem-with-my-order"
+                className={styles.support__link}
+              >
+                Center support
+              </a>
+            </div>
+
+            {/* Item 3 */}
+            <div className={styles.support__item}>
+              <h3 className={styles.support__itemTitle}>RETURN POLICY</h3>
+              <a
+                href="https://support.gymshark.com/en-US/article/how-do-i-return-my-items"
+                className={styles.support__link}
+              >
+                Return and Refund Policy
+              </a>
+            </div>
+
+            {/* Item 4 */}
+            <div className={styles.support__item}>
+              <h3 className={styles.support__itemTitle}>TECHNIQUE</h3>
+              <a
+                href="https://support.gymshark.com/en-US/article/the-gymshark-app"
+                className={styles.support__link}
+              >
+                Information about the Gymshark app
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <GoToTop />
+    </main>
+  );
 }
+
 export default Contact;
